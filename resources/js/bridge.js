0e4d95d81d6be4610b50f971c48fe7d4e63b4d3c
chuck-aka-ben <benjamin@thomasnetwork.net>
2013-11-30 14:32:25 -0500

    Using jquery cookie library

--- resources/js/bridge.js
+++ resources/js/bridge.js
@@ -13,53 +13,26 @@ function request(url, sendData, callBack, errorCallBack){
   })
 }
 
-function setCookie(c_name, value, exdays) {
-  var exdate = new Date();
-  exdate.setDate(exdate.getDate() + exdays);
-  var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-  document.cookie=c_name + "=" + c_value;
-}
-
-function getCookie(c_name) {
-  var c_value = document.cookie;
-  var c_start = c_value.indexOf(" " + c_name + "=");
-  if (c_start == -1) {
-    c_start = c_value.indexOf(c_name + "=");
-  }
-  if (c_start == -1) {
-    c_value = null;
-  }
-  else {
-    c_start = c_value.indexOf("=", c_start) + 1;
-    var c_end = c_value.indexOf(";", c_start);
-    if (c_end == -1) {
-      c_end = c_value.length;
-    }
-    c_value = unescape(c_value.substring(c_start,c_end));
-  }
-  return c_value;
-}
-
 function likeSong(id, parentEle) {
-  var cookie = getCookie("songs");
+  var cookie = $.cookie("songs");
   var songs = cookie != null ? JSON.parse(cookie) : Array();
   songs.push(id);
-  setCookie("songs", JSON.stringify(songs), 1);
+  $.cookie("songs", JSON.stringify(songs), 1);
   request("api/like", "song="+id, function(data){
     console.log("Liking song");
   });
-  parentEle.prop("disabled", true);
+  $(".like-dislike").prop("disabled", true);
 }
 
 function dislikeSong(id, parentEle) {
-  var cookie = getCookie("songs");
+  var cookie = $.cookie("songs");
   var songs = cookie != null ? JSON.parse(cookie) : Array();
   songs.push(id);
-  setCookie("songs", JSON.stringify(songs), 1);
+  $.cookie("songs", JSON.stringify(songs), 1);
   request("api/dislike", "song="+id, function(data){
     console.log("Disliking song");
   });
-  parentEle.prop("disabled", true);
+  $(".like-dislike").prop("disabled", true);
 }
 
 function queueSong(id){
@@ -151,7 +124,7 @@ function buildUpNext(parentEle, data){
 }
 
 function setButtonSongId(parentEle, data){
-  var cookie = getCookie("songs");
+  var cookie = $.cookie("songs");
   var songs = cookie != null ? JSON.parse(cookie) : Array();
   if($.inArray(data.song.id, cookie) == -1) {
     parentEle.prop("disabled", false);
