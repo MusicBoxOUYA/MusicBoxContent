df2d058f6492ae22d6c54f3f685d8209611cdf52
Andrew <andrew@lobos.me>
2013-11-30 13:58:21 -0500

    Testing api

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 var api = require("./api"),
 	express = require("express"),
-	app = express();
+	app = express(),
+	http = require("http");
 
 app.get("/", function(req,res)
 {
 	res.send("I'm up!");
+});
+
+
+app.get("/artwork", function(req,res)
+{
+	http.get("http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=0c8d0b702a23553883685199703abe8c&artist=Cher&album=Believe&format=json", function(httpres)
+	{
+		var data = "";
+		httpres.on("data", function(d)
+		{
+			data += d.toString();
+		});
+		
+		httpres.on("end", function()
+		{
+			res.json(JSON.parse(data));
+		});
+	});
 });
 
 app.use("/resources", express.static("resources"));
